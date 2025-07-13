6ec1eaec2ef159bbe43c02b5321c5f08c5ea9d34
MHA <98241106+MHA963@users.noreply.github.com>
2025-07-13 03:03:16 +0200

    update dis

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/enspyre_1/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
