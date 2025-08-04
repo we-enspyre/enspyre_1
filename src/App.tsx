4a7a607e93fd904cd7ecb785ae170a26abbf3468
MHA <98241106+MHA963@users.noreply.github.com>
2025-08-04 17:45:49 +0200

    fixing content

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// ✅ CHANGE HERE: Import HashRouter
 import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
