4913fbfca053f616c10f9ff8214758bd68a57e04
MHA <98241106+MHA963@users.noreply.github.com>
2025-08-20 19:42:38 +0200

    fixing vite.js

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
   ].filter(Boolean),
-  base: '/enspyre_1/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
